61f4ad338abc84a5d0d1fef61a5af25b24c7a508
Takashi Iwata <x.takashi.iwata.x@gmail.com>
2016-11-03 08:33:54 +0900

    Add same instance test case

--- tests/UriResolverFactoryTest.php
+++ tests/UriResolverFactoryTest.php
@@ -12,4 +12,10 @@ class UriResolverFactoryTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf(UriResolver::class, \Tkr2f\UriResolver\UriResolverFactory::get());
     }
+
+    public function testGetMethodReturnSameUriResolverInstance()
+    {
+        $uriResolver = \Tkr2f\UriResolver\UriResolverFactory::get();
+        $this->assertSame($uriResolver, \Tkr2f\UriResolver\UriResolverFactory::get());
+    }
 }
